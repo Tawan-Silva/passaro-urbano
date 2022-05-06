0edd633b267d6aab0d3ca5dceec207649a2c6eb2
Tawan-Silva <63369670+Tawan-Silva@users.noreply.github.com>
2022-05-06 20:37:09 -0300

    Add Service Ofertas e Banner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService],
 })
 export class HomeComponent implements OnInit {
+  public ofertas: Oferta[] = [];
+  constructor(private ofertasService: OfertasService) {}
 
-  constructor() { }
+  ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas();
+    console.log(this.ofertas);
 
-  ngOnInit(): void {
   }
-
 }
