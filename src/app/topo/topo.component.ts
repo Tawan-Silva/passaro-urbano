7742a5b7204a180e090bee3cb358875fd7b9f5b0
Tawan-Silva <63369670+Tawan-Silva@users.noreply.github.com>
2022-05-14 01:38:51 -0300

    Finalizando search filter react

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
+import { switchMap, debounceTime, distinctUntilChanged, catchError } from 'rxjs/operators';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
 @Component({
@@ -12,19 +12,35 @@ import { Oferta } from '../shared/oferta.model';
 export class TopoComponent implements OnInit {
 
   public ofertas!: Observable<Oferta[]>;
+  public ofertas2!: Oferta[];
   private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
     this.ofertas = this.subjectPesquisa.pipe(
+      debounceTime(1000),
+      // distict faz pesq distintas, verifica se o termo anterior
+      // é identico a novo requisição, se sim, não faz a requisição.
+      distinctUntilChanged(),
       switchMap((termo: string) => {
         console.log('requisição http para api');
+
+        if(termo.trim() === '') {
+          // retornar um observable de array fazio
+          return of<Oferta[]>([])
+        }
         return this.ofertasService.presquisaOfertas(termo)
       }))
-      this.ofertas.subscribe((ofertas: Oferta[]) => console.log(ofertas)
-      )
-  }
+      catchError((err: any) => {
+        console.log(err)
+        return of<Oferta[]>([]);
+      })
+
+      this.ofertas.subscribe((ofertas: Oferta[]) => {
+        this.ofertas2 = ofertas
+      })
+    }
 
   public pesquisa(termoDaBusca: string): void {
     console.log('keyup caracter: ', termoDaBusca);
