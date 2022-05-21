46d7660fc5d10bfe24434321cfc26ad7f7aa0bd2
Tawan Silva <tawan.tls43@gmail.com>
2022-05-21 12:06:40 -0300

    Validacao-reative forms e servico ordem-compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import {ROUTES} from './app-routes'
+
+import { CarrinhoService } from './carrinho.service';
+
+// pipe
+import { DescricaoReduzida } from './util/descricao-reduzida.pipe';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +22,6 @@ import { DiversaoComponent } from './diversao/diversao.component';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
-import { DescricaoReduzida } from './util/descricao-reduzida.pipe';
 import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component';
 import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra-sucesso.component';
 
@@ -40,10 +45,12 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(ROUTES)
+
   ],
-  providers: [ DescricaoReduzida ],
+  providers: [ CarrinhoService,
+     DescricaoReduzida ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
